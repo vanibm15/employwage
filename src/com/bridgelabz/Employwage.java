e7a0124ccc428517f5451c2056573a817da46d09
vanibm15 <vanibm15@gmail.com>
2022-04-20 17:32:21 +0530

    added:empl total wages for a month

--- src/com/bridgelabz/Employwage.java
+++ src/com/bridgelabz/Employwage.java
@@ -4,24 +4,27 @@ public class Employwage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 2;
+
 
     public static void main(String[] args) {
-
-        int empHrs = 0;
-        int empwage = 0;
-        int empcheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empcheck) {
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
+        int empHrs = 0, empWage = 0, totalEmpWage = 0;
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+            int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empcheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+            System.out.println("Emp wage: " + empWage);
         }
-        empwage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Emp wage: " + empwage);
-
+        System.out.println("Total Emp Wage:" + totalEmpWage);
     }
 }
