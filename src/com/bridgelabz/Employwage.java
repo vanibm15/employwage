21bda38701546ad17aad944654faafec02c6abbf
vanibm15 <vanibm15@gmail.com>
2022-04-20 15:14:11 +0530

    added:employ attendence

--- src/com/bridgelabz/Employwage.java
+++ src/com/bridgelabz/Employwage.java
@@ -1,7 +1,12 @@
 package com.bridgelabz;
 
 public class Employwage {
-    public static void main(String[] args){
-        System.out.println("welcome to employwage");
+    public static void main(String[] args) {
+        int IS_FULL_TIME = 1;
+        double empcheck = Math.floor(Math.random() * 10) % 2;
+        if (empcheck == IS_FULL_TIME)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
     }
 }
