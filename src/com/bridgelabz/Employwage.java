e4869e0f835752fd5b680f3c5c45c7320085d1c3
vanibm15 <vanibm15@gmail.com>
2022-04-20 16:21:00 +0530

    added:parttime employ dailywages

--- src/com/bridgelabz/Employwage.java
+++ src/com/bridgelabz/Employwage.java
@@ -1,14 +1,18 @@
 package com.bridgelabz;
 
 public class Employwage {
+
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empwage = 0;
-        double empcheck = Math.floor(Math.random() * 10) % 2;
-        if (empcheck == IS_FULL_TIME)
-            empHrs = 8;
+        double empcheck = Math.floor(Math.random() * 10) % 3;
+        if (empcheck == IS_PART_TIME)
+            empHrs = 4;
+        else if (empcheck == IS_FULL_TIME)
+        empHrs = 8;
         else
             empHrs = 0;
         empwage = empHrs * EMP_RATE_PER_HOUR;
