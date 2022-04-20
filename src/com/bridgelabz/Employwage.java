40e85888759b7977fb811c50bd1e0d895c075576
vanibm15 <vanibm15@gmail.com>
2022-04-20 15:54:20 +0530

    added:calculate daily wage

--- src/com/bridgelabz/Employwage.java
+++ src/com/bridgelabz/Employwage.java
@@ -3,10 +3,16 @@ package com.bridgelabz;
 public class Employwage {
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empwage = 0;
         double empcheck = Math.floor(Math.random() * 10) % 2;
         if (empcheck == IS_FULL_TIME)
-            System.out.println("Employee is present");
+            empHrs = 8;
         else
-            System.out.println("Employee is absent");
+            empHrs = 0;
+        empwage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp wage: " + empwage);
+
     }
 }
